9df51974a86b37ed73938b44319d21ea619b2a74
Harshda25 <harshdafugare@gmail.com>
2024-06-21 18:14:03 +0530

    radius remove

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function moveCarousel(direction) {
     const track = document.querySelector('.carousel-track');
     const cardWidth = document.querySelector('.product-card').offsetWidth;
-    const gap = 30; // Adjust based on the gap between cards
+    const gap = 30;
     const moveAmount = cardWidth + gap;
 
     track.scrollBy({ left: direction * moveAmount, behavior: 'smooth' });
